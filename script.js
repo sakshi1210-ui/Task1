bd014d1b5b86da70ff3bb4a37d941cc8f56d62d9
sakshi1210-ui <moresakshi.1206@gmail.com>
2025-07-03 19:36:00 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add or remove 'scrolled' class on navbar when user scrolls
+window.addEventListener("scroll", function () {
+  const navbar = document.getElementById("navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
